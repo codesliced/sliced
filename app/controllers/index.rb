f5c4b33b30a2d17391eb96e881c9e7a6990fc811
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-22 19:56:05 -0500

    add a photo to an album and delete photos

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,6 +5,7 @@ end
 
 get '/available_albums' do
   @albums = Album.all
+
 erb :all_albums
 end
 
@@ -14,6 +15,16 @@ get '/user/:user_id' do
   @albums_by_user = @user.albums
     
 erb :user_profile
+end
+
+get '/user/album/:album_id' do
+  @album = Album.find(params[:album_id])
+  if current_user.id == @album.user_id
+    erb :modify_album
+  else
+    redirect '/available_albums'
+  end
+
 end
 
 
@@ -38,7 +49,6 @@ post '/create_album' do
   album.user_id = current_user.id
   album.save
 
-
   photo = Photo.new(params[:photo])
   photo.album_id = album.id
   photo.save
@@ -46,45 +56,60 @@ post '/create_album' do
   redirect "/album/#{album.id}"
 end
 
-post '/delete/:id' do
-    @album = Album.find(params[:id])
-    if current_user.id == @album.user_id
-      @event.destroy
-      redirect '/'
-  else
-    redirect to 'login'
-  end
-end
+# get '/delete/album/:id' do
+#     @album = Album.find(params[:id])
+#     p params
+#     # if current_user.id == @album.user_id
+#     #   @album.destroy
+#     #   redirect '/'
+#   # else
+#   #   redirect to 'login'
+#   # end
+# end
 
 
-post '/photo/delete/:id' do
+get '/photo/delete/:id' do
     @photo = Photo.find(params[:id])
-    if current_user.id == @album.user_id
-      @event.destroy
-      redirect '/'
-  else
-    redirect to 'login'
-  end
+    # if current_user.id == @album.user_id
+      @photo.destroy
+      redirect back
+  # else
+  #   redirect to 'login'
+  # end
 end
 
 post '/photo/favorite' do
-  user_id = current_user.id
-  photo_id = params[:photo_id]
-  photo = Photo.find(photo_id)
-  heart = params[:heart].to_i
-  hearts = photo.favorites
-  Favorite.create(:photo_id => photo_id,
-                  :user_id => user_id,
-                  :heart => heart)
+  if current_user
+    photo_id = params[:photo_id]
+    photo = Photo.find(photo_id)
+    heart = params[:heart].to_i
+    hearts = photo.favorites
+    Favorite.create(:photo_id => photo_id,
+                    :user_id => current_user.id,
+                    :heart => heart)
+    redirect "/album/#{photo.album_id}"
+  else
+    redirect '/login'
+  end
+end
+
+get '/album/upload/:album_id' do
+  @album = Album.find(params[:album_id])
+erb :add_photos_to_album
+end
+
+# get '/add_to_album/:album_id' do
+#   if current_user
+#     @album = Album.find
+#     erb :add_photos_to_album
+#   end
+# end
+
+
+post '/album/upload/:album_id' do
+  @album = Album.find(params[:album_id])
+  photo = Photo.new(params[:photo])
+  photo.album_id = @album.id
+  photo.save
   redirect "/album/#{photo.album_id}"
-end
-
-get '/album/:album_id/upload' do
-  
-erb :upload_photo
-end
-
-
-post '/album/:album_id/upload' do
-  
 end
